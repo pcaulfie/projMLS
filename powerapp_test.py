daf0d1d5efb06b8ef89ccb0249b5812c321c2d1a
Paul Caulfield <SESA87346@se.com>
2021-01-04 21:20:28 +0000

    got app working with server

--- powerapp_test.py
+++ powerapp_test.py
@@ -1,12 +1,13 @@
 # flask for web app.
 import flask as fl
+from flask import url_for, request, redirect, abort, jsonify, session, render_template
 # numpy for numerical work.
 import numpy as np
 import pandas as pd
 from powerapp import testSpeed
 
 # Create a new web app.
-app = fl.Flask(__name__)
+app = fl.Flask(__name__, static_url_path='', static_folder='static')
 
 # Add root route.
 @app.route("/")
@@ -19,27 +20,50 @@ def uniform():
   return {"value": np.random.uniform()}
 
 # Add normal route.
+@app.route('/api/power')
+def getPower():
+  #data = float(fl.request.args.get())
+  data = float(fl.request.get_json())
+  #x = request.args.get('windspeed')
+  #params = {"windspeed": request.json["windspeed"]}
+  # params = {'windspeed':15.75}
+  # x = params.get("windspeed")
+  # arr = testSpeed(x)
+  # # # ts = arr.astype('f')
+  # # # ans = {"value": ts}
+  # # # print(ans)
+  # # # return {"value": testSpeed(15.5)}
+  # # # print(str(ans))
+  # df = pd.DataFrame(arr)
+  # ts = df.iloc[0][0] # Access the first (only) row as a Series using iloc, and then the value using the column name
+  # return {"value": ts}
+  #return jsonify(ts)
+  #return jsonify(getPower())
+  return jsonify(data)
+
+
+# @app.route('/api/normal', methods=['GET','PUT']) # THIS WORKS
+# def getPower2():
+#   params = {'windspeed':15.75}
+#   x = params.get("windspeed")
+#   arr = testSpeed(x)
+#   df = pd.DataFrame(arr)
+#   ts = df.iloc[0][0] # Access the first (only) row as a Series using iloc, and then the value using the column name
+#   return {"value": ts}
+  
+ 
 @app.route('/api/normal')
-def normal(params):
-  #params = {'windspeed':15.75}
-  x = params.get("windspeed")
-  #print(x)
-  arr = testSpeed(x)
-  # ts = arr.astype('f')
-  # ans = {"value": ts}
-  # print(ans)
+def normal():
+  
+  # loc = float(fl.request.args.get("loc", "0.0"))
+  loc = float(fl.request.args.get("loc", "0.0"))
+  #return fl.jsonify(np.random.normal(loc, scale, size).tolist())
   # return {"value": testSpeed(15.5)}
-  # print(str(ans))
+  # params = {'windspeed':15.75}
+  # x = params.get("windspeed")
+  arr = testSpeed(loc)
   df = pd.DataFrame(arr)
   ts = df.iloc[0][0] # Access the first (only) row as a Series using iloc, and then the value using the column name
   return {"value": ts}
-  #print(ts)
- 
-# @app.route('/api/normal', methods=['PUT'])
-# def update():
-#     rates = {
-#         "windspeed": request.json["windspeed"]
-#     }
-
-#     return jsonify(ordersDao.create(order))
-#normal()
+  #return fl.jsonify(np.random.normal(loc, scale, size).tolist())
+  #return {"value": loc}
